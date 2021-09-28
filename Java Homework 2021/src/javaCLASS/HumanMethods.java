6c2b88b54f9e788176b9d5b594d3bd41667e45c8
laurievance <laurievance@10.1.68.41>
2021-09-28 08:52:08 -0400

    CW

---
+++ Java Homework 2021/src/javaCLASS/HumanMethods.java
@@ -0,0 +1,87 @@
+package javaCLASS;
+
+public class HumanMethods {
+
+	private double age, gender;
+	
+	// an example method to use as a guideline
+	public void age(int x) {
+		age = x;
+	}
+	public void gender(char y) {
+		gender = y;
+	}
+	
+	public void canVote() {
+		if (age >= 18) {
+			System.out.println("yes you can vote");
+		}
+		else if (age < 18){
+			System.out.println("no you cannot vote");
+		}
+	}
+		
+	public void needsTetnus() {
+		if (age % 4 == 0) {
+			System.out.println("yes, you need a tetnus shot");
+		}
+		else {
+			System.out.println("no, you do not need a tetnus shot");
+		}
+	}
+	
+	public void ifToddler() {
+		if (age < 4 && gender == 'g') {
+			System.out.println("Toddler Girl");
+		}
+		else if (age < 4 && gender == 'b') {
+			System.out.println("Toddler Boy");
+		}
+		else {
+			System.out.println("Not a Todler");
+		}
+	}
+	
+	public void movieDiscount() {
+		if (age < 12 || age > 65) {
+			System.out.println("yes you get a discount");
+		}
+		else {
+			System.out.println("no discount");
+		}		
+	}
+	public void ifTeenager() {
+		if (age >= 12 && age <=18) {
+			System.out.println("teenager");
+		}
+		else {
+			System.out.println("not a teenager");
+		}		
+	}
+	
+
+
+
+		public static void main(String[] args) {	
+			
+			HumanMethods tester = new HumanMethods();
+			
+			tester.age(4);
+			
+			tester.gender('b');
+			
+			tester.canVote();
+			
+			tester.needsTetnus();
+			
+			tester.ifToddler();
+			
+			tester.movieDiscount();
+			
+			tester.ifTeenager();
+					
+	
+	
+		}
+}
+	
