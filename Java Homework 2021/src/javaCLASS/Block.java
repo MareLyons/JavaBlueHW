55ace30f7a81f6a101bd431a7cff5840e16efb5f
laurievance <laurievance@10.1.231.210>
2022-03-29 09:40:31 -0400

    march

---
+++ Java Homework 2021/src/javaCLASS/Block.java
@@ -0,0 +1,39 @@
+package javaCLASS;
+
+import java.awt.Graphics;
+import java.awt.Color;
+
+public class Block extends ListFallingBlocks{
+	
+private int x, y, speed;
+private Color color;
+
+
+//	Create a Block class. This class should hold 4 instance variables - x, y, color, and speed. 
+//	The class should have at least 3 methods: a constructor, draw(Graphics g), and move(). I'll 
+//	let you figure out what these methods should do. 
+
+
+	public Block(int x, int y, Color color, int speed) {
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		this.speed = speed;
+	
+	}
+	
+	public void draw(Graphics g){
+		g.setColor(color);
+		g.fillRect(x, y, 6, 6);
+	}
+	
+	public void move() {
+		y += speed;
+	}
+	
+	public int getY(){
+		return(y);
+	}
+	
+
+}
